d46f84d8c395ec69111964217566b3ea3ec01082
Iker <iker2chiflo@gmail.com>
2022-02-21 19:07:47 +0100

    calibration

--- Calibrar_camara/camera_calibration.py
+++ Calibrar_camara/camera_calibration.py
@@ -21,7 +21,7 @@ number_of_squares_X = 10  # Number of chessboard squares along the x-axis
 number_of_squares_Y = 7  # Number of chessboard squares along the y-axis
 nX = number_of_squares_X - 1  # Number of interior corners along x-axis
 nY = number_of_squares_Y - 1  # Number of interior corners along y-axis
-square_size = 0.025  # Size, in meters, of a square side
+square_size = 0.024  # Size, in meters, of a square side
 
 # Set termination criteria. We stop either when an accuracy is reached or when
 # we have finished a certain number of iterations.
